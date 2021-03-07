77ad714075217a386512b94f0d26e20950ec9ad1
Omar Flores <omar.garciaf247@gmail.com>
2021-03-06 19:09:29 -0500

    creating fab

---
+++ components/FAB.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, TouchableHighlight, Image } from "react-native";
+import { colors } from "../styles/colors";
+import { typography } from "../styles/typography";
+
+const FAB = ({}) => {};
+
+const FABStyles = StyleSheet.create({});
+
+export default FAB;
