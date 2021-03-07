10c477ff5eec12bd95105fe4c00a47c42e2334e8
Omar Flores <omar.garciaf247@gmail.com>
2021-03-07 14:39:23 -0500

    making header component

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,60 @@
-import { StyleSheet, View } from "react-native";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { typography } from "../styles/typography";
+import Fab from "./Fab";
 
 const Header = ({
-  back = false,
+  text,
   options = false,
   handleOptions = null,
-  handleBack = null,
+  cancelText = "",
+  cancel = false,
+  handleCancel = null,
 }) => {
-  return <View></View>;
+  return (
+    <View style={HeaderStyles.container}>
+      {cancel && (
+        <TouchableHighlight style={{ padding: 16 }} onPress={handleCancel}>
+          <Text style={typography.detail}>{cancelText}</Text>
+        </TouchableHighlight>
+      )}
+      <View
+        style={[
+          HeaderStyles.content,
+          { justifyContent: options ? "space-between" : "flex-start" },
+        ]}
+      >
+        <Text style={typography.header2}>{text}</Text>
+        {options && (
+          <Fab
+            onPress={handleOptions}
+            action="options"
+            secondary
+            color="lime"
+          ></Fab>
+        )}
+      </View>
+    </View>
+  );
 };
 
 const HeaderStyles = StyleSheet.create({
-  container: {},
+  container: {
+    display: "flex",
+    height: 124,
+    width: "100%",
+    alignItems: "flex-start",
+    justifyContent: "flex-end",
+  },
+  content: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "flex-start",
+    height: 52,
+    width: "100%",
+    paddingHorizontal: 16,
+  },
 });
 
 export default Header;
--- components/Home.js
+++ components/Home.js
@@ -3,16 +3,23 @@ import { Text, StyleSheet, View } from "react-native";
 import { typography } from "../styles/typography";
 import Button from "./Button";
 import Fab from "./Fab";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <View
       style={{
         height: "100%",
-        padding: 20,
       }}
     >
-      <Text style={typography.display}>This is home</Text>
+      <Header
+        options
+        handleOptions={() => {}}
+        cancel
+        cancelText="back"
+        handleCancel={() => {}}
+        text="header"
+      />
       <Button text="test" color="lime" onPress={() => {}} />
       <Fab action="new" color="lime" />
     </View>
