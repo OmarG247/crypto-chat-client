c244e04730aeb856a447a1a3671d03e10ec1b944
Omar Flores <omar.garciaf247@gmail.com>
2021-03-11 23:21:22 -0500

    home eidits

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { colors } from "../styles/colors";
 import { containers } from "../styles/containers";
-import { typography } from "../styles/typography";
 import Button from "./Button";
 import Contact from "./Contact";
 import Fab from "./Fab";
@@ -17,33 +16,33 @@ const sampleContact = {
   color: colors.tealSecondary,
 };
 
-const Home = ({ navigation }) => {
-  return (
-    <View style={containers.parent}>
-      <Header
-        options
-        handleOptions={() => {}}
-        cancel
-        cancelText="back"
-        handleCancel={() => { navigation.navigate('TestNav') }}
-        text="Messages"
-      />
-      <ScrollView style={containers.basic}>
-        <Option text="test" onPress={() => {}} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Contact onPress={() => {}} newMessage contact={sampleContact} />
-        <Spacer />
-      </ScrollView>
-      {/* <Button text="test" color="lime" onPress={() => {}} /> */}
-      {/* <Fab action="new" color="lime" /> */}
-      <Footer action="new" handleAction={() => {}} />
-    </View>
-  );
-};
+const Home = ({ navigation }) => (
+  <View style={containers.parent}>
+    <Header
+      options
+      handleOptions={() => {}}
+      cancel
+      cancelText="back"
+      handleCancel={() => {
+        navigation.navigate("TestNav");
+      }}
+      text="Messages"
+    />
+    <ScrollView style={containers.basic}>
+      <Option text="test" onPress={() => {}} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Contact onPress={() => {}} newMessage contact={sampleContact} />
+      <Spacer />
+    </ScrollView>
+    {/* <Button text="test" color="lime" onPress={() => {}} /> */}
+    {/* <Fab action="new" color="lime" /> */}
+    <Footer action="new" handleAction={() => {}} />
+  </View>
+);
 
 export default Home;
