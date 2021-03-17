33dfa7364afbdbd99f5dd34c5800c516b4141acc
Omar Flores <omar.garciaf247@gmail.com>
2021-03-17 01:34:00 -0400

    resizing fonts

--- styles/typography.js
+++ styles/typography.js
@@ -1,39 +1,43 @@
 import { StyleSheet } from "react-native";
 import { colors } from "./colors";
 
+/**
+ *? Every font size is equal to the design system's size + 2 for scalability
+ */
+
 export const typography = StyleSheet.create({
   display: {
-    fontSize: 48,
+    fontSize: 50,
     fontFamily: "DMSans-Regular",
     color: colors.light,
   },
   header1: {
-    fontSize: 32,
+    fontSize: 34,
     fontFamily: "DMSans-Regular",
     color: colors.light,
   },
   header2: {
-    fontSize: 24,
+    fontSize: 26,
     fontFamily: "DMSans-Regular",
     color: colors.light,
   },
   subtitle: {
-    fontSize: 20,
+    fontSize: 22,
     fontFamily: "DMSans-Regular",
     color: colors.light,
   },
   button: {
-    fontSize: 16,
+    fontSize: 18,
     fontFamily: "DMSans-Medium",
     color: colors.light,
   },
   detail: {
-    fontSize: 12,
+    fontSize: 14,
     fontFamily: "DMSans-Regular",
     color: colors.light,
   },
   body: {
-    fontSize: 16,
+    fontSize: 18,
     fontFamily: "SourceSansPro-Regular",
     color: colors.light,
   },
