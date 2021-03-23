1db9753b659ec927c51272f8fda9de06aa885cc0
Omar Flores <omar.garciaf247@gmail.com>
2021-03-23 18:50:56 -0400

    adding QR code generation

--- screens/NewContact.js
+++ screens/NewContact.js
@@ -9,6 +9,7 @@ import Input from "../components/Input";
 import ColorPicker from "../components/ColorPicker";
 import Button from "../components/Button";
 import Icon from "../components/Icon";
+import QRKeyModal from "../components/QRKeyModal";
 
 const NewContact = ({ navigation }) => {
   const [userInfo, setUserInfo] = useState({
@@ -17,6 +18,8 @@ const NewContact = ({ navigation }) => {
     color: colors.limeAccent,
   });
   const [keyScanned, setKeyScanned] = useState(false);
+  const [keyModalOpen, setKeyModalOpen] = useState(false);
+  const key = "https://omarflores.dev";
 
   const handleInput = (type, input) => {
     setUserInfo({ ...userInfo, [type]: input });
@@ -31,60 +34,65 @@ const NewContact = ({ navigation }) => {
   const isReady = () => formIsValid() && keyScanned;
 
   return (
-    <View style={containers.parent}>
-      <ScrollView
-        style={containers.main}
-        contentContainerStyle={{ paddingTop: headerHeight }}
-        scrollEnabled={false}
-      >
-        <Input
-          style={NewContactStyles.block}
-          onChangeText={(input) => handleInput("firstName", input)}
-          label="First name"
-          value={userInfo.firstName}
-          error={!userInfo.firstName && "field is mandatory"}
-        />
-        <Input
-          style={NewContactStyles.block}
-          onChangeText={(input) => handleInput("lastName", input)}
-          label="Last name"
-          value={userInfo.lastName}
-          error={!userInfo.lastName && "field is mandatory"}
-        />
-        <ColorPicker color={userInfo.color} handleColor={handleColor} />
-        <View style={NewContactStyles.block}>
-          {keyScanned ? (
-            <View style={NewContactStyles.centered}>
-              <Text style={typography.detail}>connection secured</Text>
-              <Icon style={{ marginLeft: 8 }} name="lock" color="light" />
-            </View>
-          ) : (
-            <View style={NewContactStyles.centered}>
-              <Text style={typography.detail}>
-                your key has not been scanned by this contact
-              </Text>
-              <Icon style={{ marginLeft: 8 }} name="unlock" color="light" />
-            </View>
-          )}
-        </View>
-        <View style={NewContactStyles.block}>
-          <Button expanded text="scan key" />
-          <Button
-            secondary
-            expanded
-            style={{ marginTop: 12 }}
-            text="display key"
-            onPress={() => setKeyScanned(!keyScanned)}
+    <>
+      <View style={containers.parent}>
+        <ScrollView
+          style={containers.main}
+          contentContainerStyle={{ paddingTop: headerHeight }}
+          scrollEnabled={false}
+        >
+          <Input
+            style={NewContactStyles.block}
+            onChangeText={(input) => handleInput("firstName", input)}
+            label="First name"
+            value={userInfo.firstName}
+            error={!userInfo.firstName && "field is mandatory"}
           />
-        </View>
-      </ScrollView>
-      <Header
-        text="New contact"
-        cancelText="cancel"
-        handleCancel={() => navigation.goBack()}
-      />
-      <Footer actionDisabled={!isReady()} action="save" />
-    </View>
+          <Input
+            style={NewContactStyles.block}
+            onChangeText={(input) => handleInput("lastName", input)}
+            label="Last name"
+            value={userInfo.lastName}
+            error={!userInfo.lastName && "field is mandatory"}
+          />
+          <ColorPicker color={userInfo.color} handleColor={handleColor} />
+          <View style={NewContactStyles.block}>
+            {keyScanned ? (
+              <View style={NewContactStyles.centered}>
+                <Text style={typography.detail}>connection secured</Text>
+                <Icon style={{ marginLeft: 8 }} name="lock" color="light" />
+              </View>
+            ) : (
+              <View style={NewContactStyles.centered}>
+                <Text style={typography.detail}>
+                  your key has not been scanned by this contact
+                </Text>
+                <Icon style={{ marginLeft: 8 }} name="unlock" color="light" />
+              </View>
+            )}
+          </View>
+          <View style={NewContactStyles.block}>
+            <Button expanded text="scan key" />
+            <Button
+              secondary
+              expanded
+              style={{ marginTop: 12 }}
+              text="display key"
+              onPress={() => setKeyModalOpen(true)}
+            />
+          </View>
+        </ScrollView>
+        <Header
+          text="New contact"
+          cancelText="cancel"
+          handleCancel={() => navigation.goBack()}
+        />
+        <Footer actionDisabled={!isReady()} action="save" />
+      </View>
+      {keyModalOpen && (
+        <QRKeyModal key={key} onClose={() => setKeyModalOpen(false)} />
+      )}
+    </>
   );
 };
 
