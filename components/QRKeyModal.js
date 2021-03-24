b78edf93c941cf2595395209bafdf9a927ac79cb
Omar Flores <omar.garciaf247@gmail.com>
2021-03-23 22:35:10 -0400

    more styling for android

--- components/QRKeyModal.js
+++ components/QRKeyModal.js
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { BlurView } from "expo-blur";
 import QRCode from "react-native-qrcode-svg";
 import Fab from "./Fab";
+import { colors } from "../styles/colors";
+import { typography } from "../styles/typography";
 
-const QRKeyModal = ({ cipherKey, onClose }) => (
-  <BlurView intensity={90} tint="dark" style={Styles.modalContainer}>
+const QRKeyModal = ({ cipherKey, onClose }) => {
+  const content = (
     <View style={Styles.modal}>
       <QRCode value={cipherKey} size={264} />
+      <Text style={[typography.button, { marginTop: 24 }]}>
+        Have your contact scan this code
+      </Text>
       <Fab
         style={{ marginTop: 24 }}
         action="cancel"
@@ -16,18 +21,27 @@ const QRKeyModal = ({ cipherKey, onClose }) => (
         onPress={onClose}
       />
     </View>
-  </BlurView>
-);
+  );
+
+  return Platform.OS === "ios" ? (
+    <BlurView intensity={90} tint="dark" style={Styles.modalContainer}>
+      {content}
+    </BlurView>
+  ) : (
+    <View style={Styles.modalContainer}>{content}</View>
+  );
+};
 
 const Styles = StyleSheet.create({
   modalContainer: {
-    position: 'absolute',
-    width: '100%',
+    position: "absolute",
+    width: "100%",
     height: "100%",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     zIndex: 6,
+    backgroundColor: colors.darkSurface,
   },
   modal: {
     display: "flex",
