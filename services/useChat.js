7105c357688fd8761b535630d6f01ad46e139c71
Zach19 <zach.whatelse@gmail.com>
2021-03-31 23:46:09 -0400

    Sockets stuff

--- services/useChat.js
+++ services/useChat.js
@@ -1,29 +1,31 @@
 import React, {useEffect, useRef, useState} from "react";
-import io from "socket.io-client";
+import { io } from "socket.io-client";
 
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
 
-const SOCKET_SERVER_URL = 'ws://127.0.0.1:4000'
+const SOCKET_SERVER_URL = 'http://localhost:4000'
 
 const useChat = username => {
 	const [messages, setMessages] = useState({});
-    const socketRef = useRef();
 
-    useEffect(() => {
-        socketRef.current = io(SOCKET_SERVER_URL, {
-            auth: {
-                userID: username // Replace this with token eventually
-            }
+	useEffect(() => {
+		const socket = io(SOCKET_SERVER_URL, {
+			auth: {
+				userID: username // Replace this with token eventually
+			}
 		});
-
-		// console.log('socketRef', socketRef.current)
-
-		socketRef.current.on('connect', () => {
-			console.log(socketRef.current.id);
-			console.log('connected!')
-		})
-
-		socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, async ({ to, from, content }) => {
+	
+		if (!socket.connected) {
+			socket.connect()
+		}
+	
+		socket.on("connect", () => {
+			console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+		});
+	
+		console.log(socket.connected)
+	
+		socket.on(NEW_CHAT_MESSAGE_EVENT, async ({ to, from, content }) => {
 			console.log('Message: ', content);
 			setMessages(messages => {
 				const prevConversation = messages[from] || [];
@@ -32,16 +34,52 @@ const useChat = username => {
 					[from]: [...prevConversation, {content: message, fromSelf: false, time: new Date()}]
 				};
 			});
-		});
+		})
 
 		return () => {
-            socketRef.current.disconnect();
-        };
+			socket.disconnect();
+		}
+	}, [])
 
-	}, [username])
+
+
+
+    // useEffect(() => {
+	// 	console.log(socketRef)
+    //     socketRef.current = io(SOCKET_SERVER_URL, {
+    //         auth: {
+    //             userID: username // Replace this with token eventually
+    //         }
+	// 	});
+
+	// 	socketRef.current.open();
+
+	// 	// console.log('socketRef', socketRef.current)
+
+	// 	socketRef.current.on('connect', () => {
+	// 		console.log(socketRef.current.id);
+	// 		console.log('connected!')
+	// 	})
+
+	// 	socketRef.current.on(NEW_CHAT_MESSAGE_EVENT, async ({ to, from, content }) => {
+	// 		console.log('Message: ', content);
+	// 		setMessages(messages => {
+	// 			const prevConversation = messages[from] || [];
+	// 			return {
+	// 				...messages,
+	// 				[from]: [...prevConversation, {content: message, fromSelf: false, time: new Date()}]
+	// 			};
+	// 		});
+	// 	});
+
+	// 	return () => {
+    //         socketRef.current.disconnect();
+    //     };
+
+	// }, [])
 
 	const sendMessage = (to, content) => {
-		socketRef.current.emit(NEW_CHAT_MESSAGE_EVENT, {
+		socket.emit(NEW_CHAT_MESSAGE_EVENT, {
 			content: content,
 			to
 		});
@@ -56,8 +94,8 @@ const useChat = username => {
 	}
 
 	const disconnectFromSocket = () => {
-        console.log("Socket: " + socketRef.current.id + " has been disconnected.");
-        socketRef.current.disconnect();
+        console.log("Socket: " + socket.id + " has been disconnected.");
+        socket.disconnect();
     };
 
 	return { messages, sendMessage, disconnectFromSocket } 
